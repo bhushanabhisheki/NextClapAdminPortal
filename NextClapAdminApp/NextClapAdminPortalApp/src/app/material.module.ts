43f939300c571b675a3c2c43152d969597ac9064
bhushanabhisheki <bhushanabhisheki@gmail.com>
2022-04-20 20:01:58 +0530

    added the header functionality to the application

---
+++ NextClapAdminApp/NextClapAdminPortalApp/src/app/material.module.ts
@@ -0,0 +1,82 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatListModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatCardModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+  ],
+  exports: [
+    MatSliderModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatListModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatCardModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+  ],
+})
+export class MaterialModule {}
