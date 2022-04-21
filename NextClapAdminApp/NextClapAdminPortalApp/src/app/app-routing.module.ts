ed2f51bd52c6a4d8096267e22773f01e848a4b57
bhushanabhisheki <bhushanabhisheki@gmail.com>
2022-04-21 13:50:24 +0530

    added dashboard and set the scrollbar width

--- NextClapAdminApp/NextClapAdminPortalApp/src/app/app-routing.module.ts
+++ NextClapAdminApp/NextClapAdminPortalApp/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [{ path: 'auth', component: AuthComponent }];
+const routes: Routes = [{ path: 'auth', component: AuthComponent }, { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
