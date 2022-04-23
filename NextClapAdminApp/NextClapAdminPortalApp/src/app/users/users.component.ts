409b5cb78c18c14195d61bef6ba2dce3f9c76fe8
bhushanabhisheki <bhushanabhisheki@gmail.com>
2022-04-23 07:51:48 +0530

    added userService to get the list of users

--- NextClapAdminApp/NextClapAdminPortalApp/src/app/users/users.component.ts
+++ NextClapAdminApp/NextClapAdminPortalApp/src/app/users/users.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../auth/user.model';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  usersSubscription: Subscription = new Subscription();
+  userList?: User[];
+  constructor(private usersService: UsersService) {}
 
   ngOnInit(): void {
+    this.usersSubscription = this.usersService.userList.subscribe((users) => {
+      this.userList = users;
+    });
   }
-
 }
---
+++ NextClapAdminApp/NextClapAdminPortalApp/src/app/users/users.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../auth/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private url: string = 'http://localhost:4000/api/v1/user';
+  userList: Subject<User[]> = new Subject<User[]>();
+
+  constructor(private http: HttpClient) {}
+
+  getQueryList(serviceId: string) {
+    this.http.get<any>(this.url, {}).subscribe((response) => {
+      this.userList.next(response.faqList);
+    });
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    console.log(errorRes);
+    if (!errorRes.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+    return throwError(() => new Error(errorRes.error.message));
+  }
+}
