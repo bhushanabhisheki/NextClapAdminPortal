16fd3167838470c1c20dc46081b0a69034cc603a
bhushanabhisheki <bhushanabhisheki@gmail.com>
2022-04-24 07:02:55 +0530

    added form io plugin to the application

--- NextClapAdminApp/NextClapAdminPortalApp/src/app/service-spots/service-spots.component.ts
+++ NextClapAdminApp/NextClapAdminPortalApp/src/app/service-spots/service-spots.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-service-spots',
   templateUrl: './service-spots.component.html',
-  styleUrls: ['./service-spots.component.scss']
+  styleUrls: ['./service-spots.component.scss'],
 })
-export class ServiceSpotsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ServiceSpotsComponent {
+  @ViewChild('json') jsonElement?: ElementRef;
+  public form: Object = { components: [] };
+  onChange(event: any) {
+    console.log(event.form);
   }
-
+  test() {}
 }
--- NextClapAdminApp/NextClapAdminPortalApp/src/app/service-spots/service-spots.module.ts
+++ NextClapAdminApp/NextClapAdminPortalApp/src/app/service-spots/service-spots.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ServiceSpotsRoutingModule } from './service-spots-routing.module';
 import { ServiceSpotsComponent } from './service-spots.component';
 import { MaterialModule } from '../material.module';
+import { FormioModule } from '@formio/angular';
 
 @NgModule({
   declarations: [ServiceSpotsComponent],
-  imports: [CommonModule, ServiceSpotsRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    ServiceSpotsRoutingModule,
+    MaterialModule,
+    FormioModule,
+  ],
 })
 export class ServiceSpotsModule {}
